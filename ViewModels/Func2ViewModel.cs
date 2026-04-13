6dce3b0894e9d48f011edd1a3fa090de90d31e40
ivankossdev <ivan.koss@yandex.ru>
2026-04-13 13:58:56 +0300

    Func2View and Model exapmle

---
+++ ViewModels/Func2ViewModel.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.ObjectModel;
+
+namespace WinApp.ViewModels;
+
+public partial class Func2ViewModel : ViewModelBase
+{
+    
+}
---
+++ Views/Func2View.axml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WinApp.Views;
+
+public partial class Func2View : UserControl
+{
+    public Func2View()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
